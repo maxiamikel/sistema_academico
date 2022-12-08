45919a9527760d2a34d6d56319469bcb32baec0b
maxiamikel <maxloversist@gmail.com>
2022-12-08 09:58:17 -0300

    Criação do back

---
+++ back-academico/src/main/java/com/maxi/backacademico/BackAcademicoApplication.java
@@ -0,0 +1,13 @@
+package com.maxi.backacademico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackAcademicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackAcademicoApplication.class, args);
+	}
+
+}
---
+++ back-academico/src/test/java/com/maxi/backacademico/BackAcademicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maxi.backacademico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackAcademicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
